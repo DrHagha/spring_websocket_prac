7909a83696052aff6b8fd9e3da49266922f80e7f
DrHagha <coby4313@gmail.com>
2023-04-26 15:08:40 +0900

    first sommit

---
+++ src/test/java/webket/uracle_prac/UraclePracApplicationTests.java
@@ -0,0 +1,13 @@
+package webket.uracle_prac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UraclePracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
